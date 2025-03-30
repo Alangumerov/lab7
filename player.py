8f1a1069288c3a263cb22ad0419d577bdf3511e9
Alangumerov <a_gumerov@kbtu.kz>
2025-03-30 17:39:57 +0600

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,59 @@
 #2 Create music player with keyboard controller. You have to be able to press keyboard: play, stop, next and previous as some keys. Player has to react to the given command appropriately.
+import pygame
+import os
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.mixer.init()
+MUSIC_FOLDER="music"
+tracks=[f for f in os.listdir(MUSIC_FOLDER) if f.endswith(('.mp3', '.wav'))]
+if not tracks:
+    raise FileNotFoundError("Ошибка")
+current_track=0
+paused=False
+playing=False
+def play(index):
+    global paused
+    paused=False
+    playing=True
+    pygame.mixer.music.load(os.path.join(MUSIC_FOLDER, tracks[index]))
+    pygame.mixer.music.play()
+def stop():
+    pygame.mixer.music.stop()
+    playing=False
+def next():
+    global current_track
+    current_track=current_track+1
+    play(current_track)
+
+def previous():
+    global current_track
+    current_track=current_track-1
+    play(current_track)
+def pause():
+    global paused, playing
+    if pygame.mixer.music.get_busy():
+        if paused:
+            pygame.mixer.music.unpause()
+        else:
+            pygame.mixer.music.pause()
+        paused=not paused
+    elif not playing:
+        play(current_track)
+
+play(current_track)
+running=True
+while running:
+    for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+          running=False
+      elif event.type==pygame.KEYDOWN:
+          if event.key==pygame.K_SPACE:
+              pause()
+          elif event.key==pygame.K_s:
+              stop()
+          elif event.key==pygame.K_n:
+              next()
+          elif event.key==pygame.K_p:
+              previous()
+pygame.quit()
