abdceab4dc5216e9bc76131cb4b46a7ddfd4e47d
Alangumerov <a_gumerov@kbtu.kz>
2025-03-30 16:27:37 +0600

    Update mickey mouse.py

--- mickey mouse.py
+++ mickey mouse.py
@@ -1,5 +1,5 @@
 #1 Create a simple clock application (only with minutes and seconds) which is synchronized with system clock. Use Mickey's right hand as minutes arrow and left - as seconds.
-mport pygame
+import pygame
 import time
 
 
