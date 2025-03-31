c52e79f8d36c36a8fb60c05e584e0b5632fe7683
Alangumerov <a_gumerov@kbtu.kz>
2025-03-31 17:14:18 +0600

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+
+screen=pygame.display.set_mode((800,600))
+x,y=400,300
+step = 20
+red = (255, 0, 0)
+radius=25
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT and x - radius - step >=0:
+                x-=step
+            if event.key==pygame.K_RIGHT and x+radius+step<=800:
+                x+=step
+            if event.key==pygame.K_DOWN and y+radius+step<=600:
+                y+=step
+            if event.key==pygame.K_UP and y-radius-step>=0:
+                y-=step
+    screen.fill((255,255,255))
+
+
+    pygame.draw.circle(screen, red, (x,y), radius)
+    pygame.display.flip()
+pygame.quit()
+
