fc525c554c651fc638c474ac15202dcfa88397c8
Alangumerov <a_gumerov@kbtu.kz>
2025-04-05 12:28:21 +0600

    Update mickey mouse.py

--- mickey mouse.py
+++ mickey mouse.py
@@ -4,12 +4,12 @@ import time
 
 
 pygame.init()
-
+#дисплей
 WIDTH, HEIGHT = 800, 600
 CENTER = (WIDTH // 2, HEIGHT // 2)
 BG_COLOR = (255, 255, 255)
 
-
+#картинки
 clock_face = pygame.image.load('images/photo_5373113437619612369_x (1).jpg')
 hand_for_minutes = pygame.image.load('images/photo_5373113437619612371_x__1_-removebg-preview.png')
 hand_for_seconds = pygame.image.load('images/photo_5373113437619612372_x__1_-removebg-preview.png')
@@ -20,7 +20,7 @@ clock_face = pygame.transform.scale(clock_face, (800, 600))
 
 screen = pygame.display.set_mode((800, 600))
 
-
+#функция для поворота изоображения на определенный угол
 def rotate_image(image, angle, position):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=position)
@@ -31,12 +31,12 @@ def main():
     while running:
         screen.fill(BG_COLOR)
         screen.blit(clock_face, (0, 0))
-
+         #текущее время
         t = time.localtime()
         minutes = t.tm_min
         seconds = t.tm_sec
 
-
+        #угол поворота 
         min_angle = -(minutes * 6) - 90
         sec_angle = -(seconds * 6) - 90
 
